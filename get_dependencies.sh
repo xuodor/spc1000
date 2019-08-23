dffc86b84b3c4f650aa5bbe97fb379a881fc5017
Jinsuk Kim <xuodor@gmail.com>
2019-08-23 13:12:44 +0900

    Remove dependency on SDL_image
    SDL_image is not used any longer for the project. Removes the package
    retrieval and dependencies in the scripts.

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -7,12 +7,6 @@ unzip -q SDL2-2.0.8.zip
 mv SDL2-2.0.8 SDL2
 rm SDL2-2.0.8.zip
 
-wget https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.3.zip
-unzip -q SDL2_image-2.0.3.zip
-mv SDL2_image-2.0.3 SDL2_image
-rm SDL2_image-2.0.3.zip
-popd
-
 pushd external
 wget https://github.com/native-toolkit/tinyfiledialogs/archive/master.zip
 unzip -q master.zip
